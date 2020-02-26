6d5547fe508551701dbed97dace36557484ccaf7
Kiëd Llaentenn <kiedtl@protonmail.com>
2020-02-26 18:43:18 -0500

    upddb.v: switch to sqlite

--- lib/upddb.v
+++ lib/upddb.v
@@ -5,19 +5,34 @@
 */
 
 import os
+import sqlite
 
 fn main() {
 	lines := os.read_lines("./unicode.txt")?
+	len := lines.len
+	db := sqlite.connect("./chars.db")
 
-	for line in lines {
+	// we're assuming that the makefile has already
+	// purged the file
+	db.exec_none("CREATE TABLE map (id int, description varchar(255));")
+
+	for i, line in lines {
+		print("\rstoring description for character $i/$len")
+
 		data := line.split(";")
 		mut desc := data[1]
 
-		if desc == "<control>" {
-			desc = data[11]
+		// the descriptions for control characters
+		// aren't really descriptions
+		if i < 32 {
+			desc = data[10]
 		}
 
-		print(rune(desc.len))
-		print(desc)
+		// descriptions in unicode.txt are uppercase
+		desc = desc.to_lower()
+
+		db.exec_none("INSERT INTO map VALUES ($i, '$desc');")
 	}
+
+	print("\n")
 }
