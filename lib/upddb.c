fd342feca5df72e09fb2abd6eb6842cdeb89a50a
Kiëd Llaentenn <kiedtl@tilde.team>
2021-01-20 01:45:45 +0000

    upddb.c: minor style changes

--- lib/upddb.c
+++ lib/upddb.c
@@ -5,58 +5,59 @@
 */
 
 #include <ctype.h>
+#include <err.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <sqlite3.h>
 
+#define SQL(...) #__VA_ARGS__
+
 int
 main(void)
 {
 	FILE *unicode_txt;
-	if ((unicode_txt = fopen("./unicode.txt", "r")) == NULL) {
-		fprintf(stderr, "error: can't open ./unicode.txt.\n");
-		return 1;
-	}
+	if ((unicode_txt = fopen("./unicode.txt", "r")) == NULL)
+		err(1, "error: can't open ./unicode.txt");
 
-	char *err = NULL;
+	char *sqlerr = NULL;
 	sqlite3 *db;
-	int rc = sqlite3_open("./chars.db", &db);
-	if (rc) {
-		fprintf(stderr, "error: can't open ./chars.db for writing.\n");
-		return 1;
-	}
+	if (sqlite3_open("./chars.db", &db))
+		err(1, "error: can't open ./chars.db for writing.");
 
-	/*
-	 * we're assuming that the makefile has already
-	 * purged the file
-	 */
-	sqlite3_exec(db, "CREATE TABLE map (id INTEGER PRIMARY KEY, description TEXT);",
-		NULL, NULL, &err);
+	/* we're assuming that the makefile has already purged the file */
+	sqlite3_exec(db,
+		SQL(
+			CREATE TABLE map (id INTEGER PRIMARY KEY, description TEXT);
+		),
+	NULL, NULL, &sqlerr);
+	if (sqlerr) errx(1, "sqlite3_exec1: %s\n", sqlerr);
 
 	/* wrap inserts in transaction to improve insert speed */
-	sqlite3_exec(db, "PRAGMA synchronous = OFF", NULL, NULL, &err);
-	sqlite3_exec(db, "PRAGMA journal_mode = MEMORY", NULL, NULL, &err);
-	sqlite3_exec(db, "BEGIN TRANSACTION", NULL, NULL, &err);
+	sqlite3_exec(db, SQL(PRAGMA synchronous = OFF), NULL, NULL, &sqlerr);
+	if (sqlerr) errx(1, "sqlite3_exec2: %s\n", sqlerr);
+	sqlite3_exec(db, SQL(PRAGMA journal_mode = MEMORY), NULL, NULL, &sqlerr);
+	if (sqlerr) errx(1, "sqlite3_exec3: %s\n", sqlerr);
+	sqlite3_exec(db, SQL(BEGIN TRANSACTION), NULL, NULL, &sqlerr);
+	if (sqlerr) errx(1, "sqlite3_exec4: %s\n", sqlerr);
 
-	/* pre-compile sql queries */
-	usize insertq_len = (sizeof("INSERT INTO map VALUES (") - 1)
-			+ (sizeof("32841") - 1) /* 32841 = num of chars */
-			+ (sizeof(", '") - 1)
-			+ 90 + (sizeof("');") - 1);
-	char insertq[insertq_len];
-	sprintf((char*) &insertq, "INSERT INTO map VALUES (?1, ?2);");
+	ssize_t sqlerr2;
+
+	char *insertq = SQL(
+		INSERT INTO map VALUES (:c, :d);
+	);
+
 	sqlite3_stmt *insert_stmt;
-	sqlite3_prepare_v2(db, insertq, insertq_len, &insert_stmt, NULL);
+	sqlerr2 = sqlite3_prepare_v2(db, insertq, strlen(insertq), &insert_stmt, NULL);
+	if (sqlerr2) errx(1, "sqlite3_prepare_v2: %s\n", sqlite3_errmsg(db));
 
+	ssize_t read = 0;
+	size_t len = 0, linectr = 0;
 	char *line = NULL;
-	usize len  = 0;
-	isize read = 0;
-	usize linectr = 0;
+
 	while ((read = getline(&line, (size_t*) &len, unicode_txt)) != -1) {
-		char *tmp = NULL;
-		char *data[20];
-		usize i = 0;
+		char *tmp = NULL, *data[32];
+		size_t i = 0;
 
 		/* save pointer to line so that we can free it later */
 		char *lineptr = line;
@@ -79,12 +80,12 @@ main(void)
 		 * descriptions in unicode.txt are uppercase
 		 * and as we all know, uppercase is ugly
 		 */
-		usize desclen = strlen(desc);
+		size_t desclen = strlen(desc);
 		for (i = 0; i < desclen; ++i)
 			desc[i] = tolower(desc[i]);
 
-		char id[snprintf(NULL, 0, "%d", linectr)];
-		sprintf((char*) &id, "%d", linectr);
+		char id[32];
+		sprintf((char*) &id, "%zu", linectr);
 
 		sqlite3_bind_int(insert_stmt, 1, linectr);
 		sqlite3_bind_text(insert_stmt, 2, desc, desclen, SQLITE_STATIC);
@@ -95,8 +96,11 @@ main(void)
 	}
 
 	sqlite3_finalize(insert_stmt);
-	sqlite3_exec(db, "END TRANSACTION", NULL, NULL, &err);
-	sqlite3_exec(db, "CREATE INDEX idx ON map (id);", NULL, NULL, &err);
+	sqlite3_exec(db, SQL(END TRANSACTION), NULL, NULL, &sqlerr);
+	if (sqlerr) errx(1, "error: %s\n", sqlerr);
+	sqlite3_exec(db, SQL(CREATE INDEX idx ON map (id);), NULL, NULL, &sqlerr);
+	if (sqlerr) errx(1, "error: %s\n", sqlerr);
+
 	sqlite3_close(db);
 	free(line);
 	fclose(unicode_txt);
