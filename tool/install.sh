c9d5ab2199d4c036af2125975c803c038c4fecdf
Kiëd Llaentenn <kiedtl@tilde.team>
2021-02-17 00:01:45 +0000

    tool/install.sh: remove chars.db

--- tool/install.sh
+++ tool/install.sh
@@ -4,11 +4,9 @@ die() {
     printf '%s\n' "$1"; exit 1
 }
 
-[ -z "$1" ]       && die "usage: $0 [destination]"
-[ -f "chmap" ]    || die "can't find chmap"
-[ -f "chars.db" ] || die "can't find chmap's manpage"
-[ -f "chmap.1" ]  || die "can't find chmap's db"
+[ -z "$1" ]      && die "usage: $0 [destination]"
+[ -f "chmap" ]   || die "can't find chmap"
+[ -f "chmap.1" ] || die "can't find chmap's db"
 
 install -Dm755 chmap    "$1/bin/chmap"
-install -Dm644 chars.db "$HOME/.local/share/chmap/chars.db"
 install -Dm644 chmap.1  "$1/share/man/man1/chmap.1"
